39ddeff4bfade222b9ef309881abc34165817473
Michael <michaelcaxias.dev@gmail.com>
2021-10-24 20:07:38 -0300

    Implementa componente App

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 
 export default function App() {
+  const [ image, changeImage ] = useState('')
+  const [ loading, setLoading ] = useState(true)
+
+  const fetchCats = async () => {
+    setLoading(true);
+    const request = await fetch('https://api.thecatapi.com/v1/images/search');
+    const resolve = await request.json();
+    changeImage(resolve[0].url);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchCats();
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text style={styles.text}>App de Gatos</Text>
+      { loading
+      ? <Image source={{ uri: 'https://i.imgur.com/ly4VZeN.gif' }} style={styles.image} />
+      : <Image source={{ uri: image }} style={styles.image} />
+      }
+    <Pressable style={styles.button} onPress={fetchCats}>
+      <Text style={styles.text}>Próximo</Text>
+    </Pressable>
+    <Text style={{ color: 'white' }}>Desenvolvido por Michael Caxias</Text>
     </View>
   );
 }
@@ -14,8 +38,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121211',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  image: {
+    width: 380,
+    height: 380,
+    borderRadius: 30,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#ffc107',
   },
 });
